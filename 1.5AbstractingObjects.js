afa95aa89b9c6ab67772e16d9c274b3024b61d22
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-10-26 18:45:02 +0200

    Update 1.5AbstractingObjects.js

--- 1.5AbstractingObjects.js
+++ 1.5AbstractingObjects.js
@@ -1,4 +1,4 @@
-// 2021.10.25 FROM (MIMO) 
+// 2021.10.26 FROM (MIMO) 
 /*----------------------------------*/
 // ABSTRACTING OBJECTS
 // how to create OBJECTS that are easy to understand and use in OOP

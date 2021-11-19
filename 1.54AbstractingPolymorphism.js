30eb1cef08a5e5770c557d6ecf6b807e07614f3b
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-11-19 13:59:51 +0100

    Update 1.54AbstractingPolymorphism.js

--- 1.54AbstractingPolymorphism.js
+++ 1.54AbstractingPolymorphism.js
@@ -1,6 +1,10 @@
 // 2021.11.19 FROM (MM) 
-// ABSTRACTING OBJECTS
-// POLYMORPHISM
+ABSTRACTING OBJECTS
+POLYMORPHISM => l'ensemble est à REVOIR
+/*----------------------------------*/
+// POLYMORPHYSM lets us code different implementations for a behavior
+// both "INSTANCES" and "SUBCLASSES" can override their inherited METHODS
+
 
 /*----------------------------------*/ (I)
 // a company wants to offer a cheaper streaming plan 
@@ -45,7 +49,89 @@ tesla.turnOnAC(); // => AC on
 tesla.displayWarning(); // => Battery low
 
 
+/*----------------------------------*/ (III)
+// override "Immortal" 's inherited METHOD 
+// so that INSTANCES of "Immortal" can reset their health to 100 without losing any lives
+class Player {
+  constructor() {
+    this.health = 100;
+    this.lives = 3;
+  }
+  revive() {
+    this.lives--;
+    this.health = 100;
+  }
+}
+
+class Immortal extends Player {
+revive() {
+  this.health = 100;
+  }
 
 
+/*----------------------------------*/ (IV)
+// override the correct METHOD so that calling "spend()" on an INSTANCE of "Credit"
+// will increase its "balance" PROPERTY by the amount spent
+class Card {
+  constructor(amount) {
+    this.balance = amount;
+    this.expiry = 2024;
+  }
+  spend(amount) {
+    this.balance -= amount;
+  }
+  renew() {
+  this.expiry += 4;
+  }
+}
+
+class Credit extends Card {
+  spend(amount) { //
+  this.balance = amount; //
+  }
+}
 
 
+/*----------------------------------*/ (V) 
+// what does "player.attack()" display?
+class Player {
+  attack() {
+    console.log("Take that!");
+  }
+}
+
+class Pirate extends Player {
+  attack() {
+  console.log("Argh!");
+  }
+}
+
+class Captain extends Player {
+  attack() {
+  console.log("Walk the plank!");
+  }
+}
+
+var player = new Captain();
+player.attack(); // Walk the plank!
+
+
+/*----------------------------------*/ (VI) 
+// how many implementations of "attack()" are there? => 3
+// there are THREE implementations of "attack()"
+class Player {
+  attack() {
+    console.log("Take that!");
+  }
+}
+
+class Pirate extends Player {
+  attack() {
+  console.log("Argh!");
+  }
+}
+
+var pirate = new Pirate();
+pirate.attack = function() {
+  console.log("Walk the plank!");
+}

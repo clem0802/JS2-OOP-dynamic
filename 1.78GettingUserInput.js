2c04277eff03955ca8ad2778cdf1fc5be13180d2
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-12-18 21:53:01 +0100

    Update 1.78GettingUserInput.js

--- 1.78GettingUserInput.js
+++ 1.78GettingUserInput.js
@@ -47,7 +47,7 @@ function register() {
 
 
 
-/*----------------------------------*/
+/*----------------------------------*/ (USEFUL !!)
 // knowing how to get user input, we can use it to update a webpage
 // assemble the code so that the value of "username" appears on the webpage when clicking on the Register button
 //! (HTML)
@@ -63,13 +63,69 @@ function register() {
 
 //! (JavaScript)
 function register() {
-    var username = document.getElementById("usernameInput").value; //!
-    document.getElementById("message").innerHTML = username + ", you're signed up!"; //!  ex: Isaac, you're signed up!
+    var username = document.getElementById("usernameInput").value; //
+    document.getElementById("message").innerHTML = username + ", you're signed up!"; // => Isaac, you're signed up!
 }
 
 
 
+    
+/*----------------------------------*/ (USEFUL !!)
+// display the review in the paragraph when users click on the "ADD REVIEW" button
+//! (HTML)
+<!doctype html>
+<html>
+    <body> 
+        <img src="https://mimo.app/r/amazon.png">
+        <h1>Customer Reviews</h1>
+        <input id="nameInput" type="text" placeholder="Your name">
+        <br>
+        <br>
+        <textarea id="reviewText" rows="3" cols="18" placeholder="Write your review here"></textarea>
+        <br>
+        <br>
+        <button onclick="addAReview()">ADD REVIEW</button> 
+        <p id="addedReview"></p>
+        <script src="script.js"></script>
+    </body>
+</html>
+    
+//! (JavaScript)
+function addAReview() {
+    var name = document.getElementById("nameInput").value; //
+    var review = document.getElementById("reviewText").value;
+    document.getElementById("addedReview").innerHTML = name + ": " + review; //
+}
 
+
+    
+    
+/*----------------------------------*/ (USEFUL !!) (img de MIGNION)
+// set "tweet" to the value of the "TEXTAREA" element
+//! (HTML)
+<!doctype html>
+<html>
+    <body> 
+        <img id="profilePic" src="https://mimo.app/r/profile.png" height="50">
+        <br>
+        <br>
+        <textarea id="tweetText" rows="3" cols="18" placeholder="What's happening?"></textarea>
+        <br>
+        <br>
+        <button onclick="tweetPost()">TWEET</button> 
+        <script src="script.js"></script>
+    </body>
+</html>
+    
+//! (JavaScript)
+function tweetPost() {
+    var tweet = document.getElementById("tweetText").value; //
+    console.log(tweet);
+}
+    
+
+
+    
 /*----------------------------------*/
 // accessing user input from a TEXTAREA element works the same way
 // code   .value after accessing the TEXTAREA element
@@ -205,7 +261,7 @@ function signUp() {
 
 
 
-/*----------------------------------*/
+/*----------------------------------*/ 
 // code "review" to set the content of the paragraph to "review"
 // the VARIABLE that stores the value of the TEXTAREA element
 //! (HTML)

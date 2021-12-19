e6ac72f3806c04b52c6bee76d56f4bd4c88852cf
clem0802 <88548513+clem0802@users.noreply.github.com>
2021-12-19 19:36:27 +0100

    Create 1.82AddToCart(useful).js

---
+++ 1.82AddToCart(useful).js
@@ -0,0 +1,13 @@
+// 2021.12.19 FROM (MM) 
+/*----------------------------------*/
+// DYNAMIC WEBPAGES
+// ADD-TO-CART FEATURE
+
+
+
+/*----------------------------------*/ UPVOTE => (ajouter le COMPTEUR)
+/*----------------------------------*/ VERY USEFUL !!
+/*----------------------------------*/ all code in VScode (W5)
+// (HTML)
+// (CSS)
+// (JavaScript)

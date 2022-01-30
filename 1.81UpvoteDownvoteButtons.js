4616a1cf1563fc1c926c36ab6e5e946b9e3c2aa4
clem0802 <88548513+clem0802@users.noreply.github.com>
2022-01-30 22:07:09 +0100

    Update 1.81UpvoteDownvoteButtons.js

--- 1.81UpvoteDownvoteButtons.js
+++ 1.81UpvoteDownvoteButtons.js
@@ -10,30 +10,36 @@
 // (HTML)
 <!doctype html>
 <html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <script src="script.js"></script>
+    </head>
     <body> 
         <h1>Cutey of the Year</h1>
         <img id="profilePic" src="https://mimo.app/r/profile.png">
         <p><em>Oui Oui Oui</em></p>
-        <p id="votes">3 votes</p>
-        <button id = "upvote" onclick = "upvote()">Upvote</button>
-        <button id = "downvote" onclick = "downvote()">Downvote</button>
+        <p id="votes">3 Votes</p>
+        <button id="upvote" onclick="upvote()">Upvote</button>
+        <button id="downvote" onclick="downvote()">Downvote</button>
         <script src="script.js"></script>
     </body>
 </html>
 
+/*-----------------------------------*/
 // (JavaScript)
 var counter = 3; 
 function upvote(){
-    counter = counter + 1;
-    document.getElementById("votes").innerHTML = counter + " votes";
+    counter++;
+    document.getElementById("votes").innerText = counter + " Votes"; //
 }
 function downvote(){
-    counter = counter - 1;
-    document.getElementById("votes").innerHTML = counter + " votes";
+    counter--;
+    document.getElementById("votes").innerText = counter + " Votes";
 }
 // how to make the "votes" paragraph display "counter" followed by "votes"?
 // by setting ".innerHTML" to => counter + " votes"
 
+/*-----------------------------------*/
 // (CSS)
 body {
     text-align: center;
